924dbcf0a2f95e818a58be491851c421edd49300
Dav1dCzy <zc84@uw.edu>
2022-12-08 22:18:55 -0800

    fourth attempt

--- a5/server.R
+++ a5/server.R
@@ -9,20 +9,21 @@
 
 library(shiny)
 library(dplyr)
+data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/a5-Dav1dCzy/main/owid-co2-data.csv")
+
+# co2, cement_co2, country
+# What is the percentage of cement_co2 in total co2 emission across all the counties?
+world_map <- map_data("world")
+co2_2020 <- data %>%
+  mutate(percentage = cement_co2 / co2) %>%
+  select(country, co2, cement_co2, percentage)
+
+names(co2_2020)[1] <- paste("region")
+map_percentage <- left_join(co2_2020, world_map, by = "region")
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/a5-Dav1dCzy/main/owid-co2-data.csv")
-  
-  # co2, cement_co2, country
-  # What is the percentage of cement_co2 in total co2 emission across all the counties?
-  world_map <- map_data("world")
-  co2_2020 <- data %>%
-    mutate(percentage = cement_co2 / co2) %>%
-    select(country, co2, cement_co2, percentage)
-  
-  names(co2_2020)[1] <- paste("region")
-  map_percentage <- left_join(co2_2020, world_map, by = "region")
   
   filter_range <- reactive({
     map_percentage %>%
