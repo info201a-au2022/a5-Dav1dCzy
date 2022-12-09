9d3f2218ff5f2b1ed1bf4029091b5c2635340ee1
Dav1dCzy <zc84@uw.edu>
2022-12-08 22:05:18 -0800

    first attempt

--- a5/ui.R
+++ a5/ui.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+source("https://raw.githubusercontent.com/info201a-au2022/a5-Dav1dCzy/main/a5/server.R")
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
--- analysis.R
+++ analysis.R
@@ -7,7 +7,7 @@ data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/a5-Dav1dCzy/
 
 co2_2020 <- data %>%
   mutate(percentage = cement_co2 / co2) %>%
-  select(country, co2, cement_co2, percentage)
+  select(country, year, co2, cement_co2, percentage)
 
 
   
