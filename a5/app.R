3f6881ef6d4e4979d55c1ec6fffbd828d5082ed9
Dav1dCzy <zc84@uw.edu>
2022-12-08 22:10:50 -0800

    second attempt

---
+++ a5/app.R
@@ -0,0 +1,4 @@
+library(shiny)
+source("https://raw.githubusercontent.com/info201a-au2022/a5-Dav1dCzy/main/a5/ui.R")
+source("https://raw.githubusercontent.com/info201a-au2022/a5-Dav1dCzy/main/a5/server.R")
+shinyApp(ui = ui, server = server)
