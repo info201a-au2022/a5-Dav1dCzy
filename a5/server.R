f392a5559817c92809bcc939e03779bb564619d4
Dav1dCzy <zc84@uw.edu>
2022-12-09 21:46:51 -0800

    update column name 4

--- a5/server.R
+++ a5/server.R
@@ -13,15 +13,12 @@ library(plotly)
 library(ggplot2)
 data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/a5-Dav1dCzy/main/owid-co2-data.csv")
 
-# co2, cement_co2, country
-# What is the percentage of cement_co2 in total co2 emission across all the counties?
 world_map <- map_data('world')
 co2_2020 <- data %>%
   mutate(percentage = cement_co2 / co2) %>%
   select(country, year, co2, cement_co2, percentage)
 
 names(co2_2020)[1] <- paste("region")
-#rename(co2_2020, region = country)
 map_percentage <- left_join(co2_2020, world_map, by = "region")
 
 shinyServer(function(input, output) {
